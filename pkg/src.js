d7b52f6ce21740124c434e10fda6e51cfdd71b34
rxusi <enztketuerhtynpiii@gmail.com>
2023-06-01 00:31:34 +0900

    Fixed bugs.

--- pkg/src.js
+++ pkg/src.js
@@ -1,4 +1,34 @@
-import initSync, { Tictactoe } from "./wasm_test.js";
+import initSync, { greet, Tictactoe, Point } from "./wasm_test.js";
+    
+initSync()
+.then(() => {
+    greet("World")
+});
+
+let point;
+
+const create = function() {
+    point = new Point(2, 3);
+    //ttt = Tictactoe.new(4, 4);
+}
+window.create = create;
+export {create};
+
+const show = function() {
+    point.show();
+    //ttt.show();
+}
+window.show = show;
+export {show};
+
+const sum = function() {
+    let _sum = point.sum();
+    console.log(_sum);
+}
+window.sum = sum;
+export {sum};
+
+//-----
 
 const PLAYER = 1;
 const COM = 2;
@@ -15,7 +45,7 @@ const createBoard = function() {
 
     let board = document.querySelector("#board");
     
-    ttt = new Tictactoe(cell_n, win_n, 1);
+    ttt = new Tictactoe(cell_n, cell_n, 1);
     winner = 0;
 
     let boardHTML = ttt.getBoardHTML();
